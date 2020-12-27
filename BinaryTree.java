a878dc4b51a2ff244f5c0f31109c470532d1d84e
芝芝多莓 <66757568+boobi926@users.noreply.github.com>
2020-12-27 22:49:57 +0800

    Add files via upload
    二叉树判断是否是对称树、平衡树，层序遍历

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 
 class Node{
     public char val;
@@ -34,7 +35,7 @@ public class BinaryTree {
         if(root == null){
             return;
         }
-        System.out.println(root.val+" ");
+        System.out.print(root.val+" ");
         preOrderTraversal(root.left);
         preOrderTraversal(root.riight);
     }
@@ -44,7 +45,7 @@ public class BinaryTree {
            return;
         }
         inOrderTraversal(root.left);
-        System.out.println(root.val+" ");
+        System.out.print(root.val+" ");
         inOrderTraversal(root.riight);
 
     }
@@ -55,7 +56,7 @@ public class BinaryTree {
         }
         postOrderTraversal(root.left);
         postOrderTraversal(root.riight);
-        System.out.println(root.val+" ");
+        System.out.print(root.val+" ");
     }
     //遍历思路求结点个数
     static int size = 0;
@@ -101,6 +102,110 @@ public class BinaryTree {
 
         return getLeafSize2(root.left)+getLeafSize2(root.riight);
     }
+    //子问题求解树的高度（左树高度和右树高度取最大值加一）
+      int getHeight(Node root){
+          if(root == null){
+              return 0;
+          }
+          int leftHeight = getHeight(root.left);
+          int rightHeight = getHeight(root.riight);
+          return leftHeight>rightHeight?leftHeight+1:rightHeight
+          +1;
+
+       }
+       //查找val所在结点，没有就返回null；
+      //按照根-》左-》右 顺序查找
+      //一旦找到，立即返回，不要继续在其他位置查找
+      Node find(Node root,char val){
+        if(root == null){
+            return null;
+        }
+        //1.判断根节点是否是要找的数字
+        if(root.val==val){
+            return root;
+        }
+        //2.左边递归完，返回值是否为空
+        Node left = find(root.left,val);
+        if(left!=null){
+            return left;
+        }
+        //3.右边递归
+        Node right = find(root.riight,val);
+        if(right!=null){
+            return right;
+        }
+        return null;
+      }
+
+      //求第K层的结点个数
+    int getKLevelSize(Node root,int k){
+        if(root==null){
+            return 0;
+        }else if(k==1){
+            return 1;
+        }else {
+            return getKLevelSize(root.left,k-1)+getKLevelSize(root.riight,k-1);
+        }
+    }
+      //判断这棵树是否是镜像对称树
+      public boolean isSymmetric(Node root) {
+          if(root == null){
+              return true;
+          }
+          return childSymmetric(root.left,root.riight);
+
+      }
+    public boolean childSymmetric(Node p,Node q){
+        if(p==null && q==null){
+            return true;
+        }
+        if(p==null || q==null){
+            return false;
+        }
+        if(p.val!=q.val){
+            return false;
+        }
+        return childSymmetric(p.left,q.riight) && childSymmetric(p.riight,q.left);
+    }
+     //判断是否是平衡树（每个结点的左子树与右子树的高度差的绝对值不大于1）
+    public boolean isBalanced(Node root) {
+        if(root==null){
+            return true;
+        }
+        int lHeight = Height(root.left);
+        int rHeight = Height(root.riight);
+        return Math.abs(lHeight-rHeight)<=1 && isBalanced(root.left) && isBalanced(root.riight);
+
+    }
+     int Height(Node root){
+        if(root == null){
+            return 0;
+        }
+        int lHeight = getHeight(root.left);
+        int rHeight = getHeight(root.riight);
+
+        return lHeight>rHeight?lHeight+1:rHeight+1;
+
+    }
+    //层序遍历
+    public List<Character> levelOrder(Node root) {
+        List<Character> list = new ArrayList<>();
+        if(root==null){
+            return list;
+        }
+        Stack<Node> stack = new Stack<>();
+        Node cur = root;
+         while(cur!=null || !stack.isEmpty()){
+             while(cur!=null){
+                 stack.push(cur);
+                 list.add(cur.val);
+                 cur = cur.left;
+             }
+             Node top = stack.pop();
+             cur = top.riight;
+         }
+         return list;
+    }
     public static void main(String[] args){
         BinaryTree binaryTree = new BinaryTree();
         Node root = binaryTree.buildTree();
@@ -117,5 +222,13 @@ public class BinaryTree {
 
         System.out.println("叶子结点个数："+ binaryTree.getLeafSize(root));
         System.out.println("叶子结点个数："+ binaryTree.getLeafSize2(root));
+        Node ret = binaryTree.find(root,'H');
+        System.out.println("该值的结点是："+ret.val);
+        System.out.println("第三层结点的个数是："+binaryTree.getKLevelSize(root,3));
+        System.out.println("该树是否呈镜像对称："+binaryTree.isSymmetric(root));
+        System.out.println("该树是否是平衡二叉树："+binaryTree.isBalanced(root));
+        System.out.println("层序遍历："+binaryTree.levelOrder(root));
     }
+
+
 }
