1ad355f85c4d7f233544867e7906e0b9bacb1bd8
芝芝多莓 <66757568+boobi926@users.noreply.github.com>
2020-12-30 23:42:17 +0800

    Add files via upload
    直接插入排序、希尔排序、快速排序、堆排序

---
+++ sevenSort.java
@@ -0,0 +1,187 @@
+import java.util.Arrays;
+
+public class sevenSort {
+    //插入排序分为直接插入排序和希尔排序
+    /*
+    时间复杂度：
+     （1）最坏情况是O（n^2）
+      (2)最好情况是有序情况：O（n） 越有序越快
+    空间复杂度：O（1）
+    稳定性：稳定
+     */
+    public static void insertSort(int[] array){
+        int tmp = 0;
+        int j ;
+        for(int i=1; i<array.length; i++){
+             tmp = array[i];
+            for( j=i-1; j>=0; j--){
+                if(array[j] > tmp){
+                    array[j+1] = array[j]; //调换位置
+                }else{
+                   // array[j+1] = tmp;   //不用换，放回原位
+                    break;
+                }
+            }
+            array[j+1] = tmp;   //不用换，放回原位
+        }
+    }
+
+    //希尔排序 (直接插入排序时间复杂度太高，希尔排序采用分组的思想，对每组进行直接插入排序)
+    //不稳定
+    public static void shellSort(int[] array){
+        int[] drr = {5,3,1};
+        for(int i = 0; i<drr.length; i++){
+            shell(array,drr[i]);
+        }
+    }
+    public static void shell(int[] array,int gap){
+       int tmp = 0;
+       for(int i = gap; i<array.length; i++){
+           tmp = array[i];
+           int j;
+           for(j=i-gap; j>=0; j-=gap){
+               if(array[j]>tmp){
+                   array[j+gap] = array[j];
+               }else {
+                   break;
+               }
+           }
+           array[j+gap]=tmp;
+       }
+
+    }
+
+    //选择排序
+    /*
+    时间复杂度：O（n^2）
+    空间复杂度：O（1）
+    稳定性：不稳定
+     */
+    public static void selectSort(int[] array){
+        for(int i=0; i<array.length; i++){
+            for(int j=i+1; j<array.length; j++){
+                if(array[j]<array[i]){
+                    int tmp = array[i];
+                    array[i] = array[j];
+                    array[j] = tmp;
+                }
+            }
+
+            }
+
+    }
+
+    //堆排序（首先要建立大跟堆）
+    /*
+      时间复杂度：O（nlogn）
+    空间复杂度：O（1）
+    稳定性：不稳定
+     */
+    public static void heapSort(int[] array){
+        creatHeap(array);
+        int end = array.length-1;
+        while(end>0){
+            int tmp = array[end];
+            array[end] = array[0];
+            array[0] = tmp;
+            adjustDown(array,0,end);
+            end--;
+        }
+    }
+    public static void creatHeap(int[] array){
+        for(int i = (array.length-1-1)/2; i>=0; i--){
+            adjustDown(array,i,array.length);
+        }
+    }
+    public static void adjustDown(int[] array,int root,int len){
+        int patrnt = root;
+        int child = root*2+1;
+        //最起码有左孩子
+        while(child<len){
+            //有右孩子且左孩子小于右孩子，进行交换
+            if(child+1<len && array[child]<array[child+1]){
+             int tmp = array[child];
+             array[child] = array[child+1];
+             array[child+1] = tmp;
+            }
+            //如果左孩子大于父亲结点，要进行交换
+            if (array[child] > array[patrnt]) {
+                int tmp = array[child];
+                array[child] = array[patrnt];
+                array[patrnt] = tmp;
+                patrnt = child;
+                child = patrnt*2+1;
+            }else{
+                break;
+            }
+        }
+    }
+
+    //冒泡排序
+     /*
+      时间复杂度：O（n^2）
+    空间复杂度：O（1）
+    稳定性：不稳定
+     */
+    public static void bubleSort(int[] array){
+        //i表示趟数
+        for(int i=0; i<array.length-1; i++){
+            for(int j=0; j<array.length-1-i; j++){
+                if(array[j]>array[j+1]){
+                    int tmp = array[j];
+                    array[j] = array[j+1];
+                    array[j+1] = tmp;
+                }
+            }
+        }
+    }
+
+    //快速排序
+    /*
+      时间复杂度：nlogn     最坏情况：1 2 3 4 5 6 7 8 9 / 9 8 7 6 5 4 3 2 1  ：O（n^2）
+    空间复杂度：lonn O（n）
+    稳定性：不稳定
+     */
+    public static void quickSort(int[] array){
+        quick(array,0,array.length-1);
+    }
+    public static void quick(int[] array,int left,int right){
+        if(left>=right){
+            return;
+        }
+       int par = partition(array,left,right);
+        //递归排序左边和右边
+        quick(array,left,par-1);
+        quick(array,par+1,right);
+
+    }
+    public static int partition(int[] array,int left,int right){
+        int tmp = array[left];
+        while(left<right){
+
+            while(left<right && array[right]>=tmp){
+                right--;
+            }
+            array[left]=array[right];
+            while(left<right && array[left]<=tmp) {
+                left++;
+            }
+            array[right] = array[left];
+        }
+        array[left] = tmp;
+        return left;
+    }
+    public static void main(String[] args){
+        int[] array = {10,5,8,4,1,9};
+        System.out.println(Arrays.toString(array));
+        insertSort(array);
+        System.out.println(Arrays.toString(array));
+        selectSort(array);
+        System.out.println(Arrays.toString(array));
+        heapSort(array);
+        System.out.println(Arrays.toString(array));
+        System.out.println("快速排序：");
+        quickSort(array);
+        System.out.println(Arrays.toString(array));
+    }
+}
