d33dc0970cd9d0c015ce52dc841bed59e572fdf6
芝芝多莓 <66757568+boobi926@users.noreply.github.com>
2021-01-13 23:17:51 +0800

    Add files via upload
    两个栈实现队列

---
+++ stackIntoQueue.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+public class stackIntoQueue {
+    Stack<Integer> stack1 = new Stack<Integer>();
+    Stack<Integer> stack2 = new Stack<Integer>();
+
+    public void push(int node) {
+        stack1.push(node);
+    }
+
+    public int pop() {
+        if(empty()){
+            return -1;
+        }
+        if(stack2.isEmpty()){
+            while(!stack1.isEmpty()){
+                stack2.push(stack1.pop());
+            }
+        }
+        return stack2.pop();
+
+    }
+    public boolean empty(){
+        if(stack1.isEmpty() && stack2.isEmpty()){
+            return true;
+        }
+        return false;
+    }
+}
