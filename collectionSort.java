f71c726903bb44a8455549161f5f1b897a1250dc
芝芝多莓 <66757568+boobi926@users.noreply.github.com>
2021-01-22 21:50:19 +0800

    Add files via upload

---
+++ collectionSort.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class collectionSort {
+    public static void main(String args[]) {
+      /*  List<Integer> list = new ArrayList<>();
+        list.add(12);
+        list.add(16);
+        list.add(56);
+        list.add(23);
+        System.out.println(list);
+        //排序
+        Collections.sort(list); */
+
+        String[] strs ={"flower","flow","flight"};
+        for(int i=0;i<strs.length;i++){
+            System.out.println(strs[i]);
+        }
+
+
+    }
+}
