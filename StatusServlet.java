fb22fddcbb34f09af276933fde8996dbf29a8065
芝芝多莓 <66757568+boobi926@users.noreply.github.com>
2021-01-26 20:06:16 +0800

    Add files via upload

---
+++ StatusServlet.java
@@ -0,0 +1,19 @@
+package bobi;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/status")
+public class StatusServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.setStatus(404);
+        resp.setContentType("text/plain:charset=utf-8");
+        resp.getWriter().println("你好");
+    }
+}
